287b327a5aa6fa3684c3428ecc68107610a496be
inpathos <68312245+inpathos@users.noreply.github.com>
2021-02-17 23:23:58 -0300

    Update e08.java

--- 2021.02.15-sintaxe/e08.java
+++ 2021.02.15-sintaxe/e08.java
@@ -1,11 +1,10 @@
-matrizimport java.util.Scanner;
+import java.util.Scanner;
 
 
 class quadradoMagico {
     private static Boolean isQuadradoMagico(int[][] matriz, int dimensao){
         int i, j, linha, coluna, diagonal1 = 0, diagonal2 = 0;
         for(i = 0; i < dimensao; i++) diagonal1 += matriz[i][i];
-        System.out.println("teste1");
         for(i = 0; i < dimensao; i++){
             linha = 0;
             for(j = 0; j < dimensao; j++){
@@ -14,9 +13,7 @@ class quadradoMagico {
             }
             if(linha != diagonal1) return false;
         }
-        System.out.println("teste2");
         if(diagonal2 != diagonal1) return false;
-        System.out.println("teste3");
         for(j = 0; j < dimensao; j++){
             coluna = 0;
             for(i = 0; i < dimensao; i++) {
@@ -24,13 +21,13 @@ class quadradoMagico {
             }
             if(coluna != diagonal1) return false;
         }
-        System.out.println("teste4");
         return true;
     }
 
     private static int[][] criarQuadradoMagico(int dimensao){
         int i, j;
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite os elementos do quadrado: ");
         int[][] matriz = new int[dimensao][dimensao];
         for(i = 0; i < dimensao; i++){
             for(j = 0; j < dimensao; j++){
@@ -43,27 +40,32 @@ class quadradoMagico {
     private static void imprimeQuadradoMagico(int[][] matriz, int dimensao){
         int i, j;
         for(i = 0; i < dimensao; i++) {
-            System.out.print("\n\t");
+            System.out.print("\t");
             for(j = 0; j < dimensao; j++) {
                 System.out.print(matriz[i][j] + "\t");
             }
+            System.out.print("\n");
         }
-        System.out.print("\n");
+
     }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int n, i, j;
+        int dimensao, i, j;
         int[][] matriz;
-        n = teclado.nextInt();
+        System.out.println("Digite a dimensão do quadrado: ");
+        dimensao = teclado.nextInt();
         matriz = criarQuadradoMagico(dimensao);
+        System.out.println("\nO quadrado digitado é:\n");
         imprimeQuadradoMagico(matriz, dimensao);
-        System.out.println(isQuadradoMagico(matriz, dimensao));
+        System.out.println("\nÉ quadrado mágico? " + isQuadradoMagico(matriz, dimensao));
+
     }
 
 }
 
 /*
+
     Um quadrado mágico é uma matriz quadrada, de valores inteiros,
     na qual a soma dos elementos de cada linha, a soma dos elementos
     de cada coluna e a soma dos elementos da diagonal primária e
@@ -75,7 +77,6 @@ class quadradoMagico {
     9   7   6   12
     5   11  10  8
     16  2   3   13
-
 
     Desenvolva um programa que leia do usuário uma matriz quadrada de ordem n
     e imprima se a matriz é um quadrado mágico ou não.
